27adc3dd62422ca963f954803a11edc2f3df51ef
Sam Ragucci <samr742@gmail.com>
2025-08-20 23:26:48 -0700

    test getting data from Strapi server

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,26 @@
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+async function loader() {
+  const path = "/api/home-page";
+  const BASE_URL = "http://localhost:1337";
+  const url = new URL(path, BASE_URL);
+  const response = await fetch(url.href);
+  const data = await response.json();
+    console.log(data);
+
+  return {...data.data};
+}
+
+export default async function HomeRoute() {
+  const data = await loader();
   return (
     <main className="container mx-auto py-6">
-      <Button>Button to Nowhere</Button>
+      <h1>{data.title}</h1>
     </main>
   );
 }
+
+
+/*
+      <Button>Button to Nowhere</Button>
+*/
