82d48caf2f1f62b803abe27a0b5e845cf31f0c9e
Sam Ragucci <samr742@gmail.com>
2025-08-22 13:55:11 -0700

    install qs and its types

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,18 +1,39 @@
-import { Button } from "@/components/ui/button";
+import qs from "qs";
 
-async function loader() {
-  const path = "/api/home-page";
+const homePageQuery = qs.stringify({
+  	populate: {
+      blocks: {
+        on: {
+          "blocks.hero-section": {
+            populate: {
+              image: true,              
+              cta: true,
+            },
+          },
+        },
+      },
+    },
+});
+
+
+async function getHomePageData() {
   const BASE_URL = "http://localhost:1337";
+  const path = "/api/home-page";
   const url = new URL(path, BASE_URL);
-  const response = await fetch(url.href);
-  const data = await response.json();
-    console.log(data);
+  url.search = homePageQuery;
 
-  return {...data.data};
+  try {
+    const response = await fetch(url.href);
+    const data = await response.json();
+      console.log(data);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export default async function HomeRoute() {
-  const data = await loader();
+  const data = await getHomePageData();
   return (
     <main className="container mx-auto py-6">
       <h1>{data.title}</h1>
