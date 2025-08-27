22eec1c2c9c21ffe05fd1466853fb275fea2f92a
Sam Ragucci <samr742@gmail.com>
2025-08-27 11:40:01 -0700

    router.push redirection for NavBar links

--- client/src/app/newsletter/page.tsx
+++ client/src/app/newsletter/page.tsx
@@ -1,9 +1,9 @@
 'use client';
 import { useRouter } from 'next/navigation';
 
-const Contact = () => {
+const Newsletter = () => {
     const router = useRouter();
     router.push('https://share-na2.hsforms.com/18bwXAlo4TrqFQnxcVBLYYA40bup4');
 }
 
-export default Contact
+export default Newsletter
