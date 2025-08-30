6afdf98a0a5089768defbfff16f213c058a29f56
Sam Ragucci <samr742@gmail.com>
2025-08-29 19:18:06 -0700

    avoid dev mode console error from double-invoke

--- client/src/app/contact/page.tsx
+++ client/src/app/contact/page.tsx
@@ -2,7 +2,8 @@
 import { useRouter } from 'next/navigation';
 
 const Contact = () => {
-    const router = useRouter();
+  const router = useRouter();
+  if (typeof window !== "undefined")
     router.push('https://share-na2.hsforms.com/18bwXAlo4TrqFQnxcVBLYYA40bup4');
 }
 
