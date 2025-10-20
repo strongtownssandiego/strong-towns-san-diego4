def1a652e5b05796d2eb5c86d16ab09524b2561b
Sam Ragucci <samr742@gmail.com>
2025-10-20 11:19:16 -0700

    back to local server for now

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -6,6 +6,6 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function getStrapiURL() {
-  return process.env.STRAPI_URL ?? "http://localhost:1337";
-  // return process.env.STRAPI_API_URL ?? "http://localhost:1337";
+  return "http://localhost:1337";
+  // return process.env.STRAPI_CLOUD_URL ?? "http://localhost:1337";
 }
