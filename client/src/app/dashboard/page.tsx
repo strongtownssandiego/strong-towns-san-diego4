1ebb57c87b2b510c081b2ea08f4a7dc888427f04
Sam Ragucci <samr742@gmail.com>
2025-12-05 11:25:58 -0800

    check whether user is logged in

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,7 +1,13 @@
-export default function DashboardRoute() {
+import { cookies } from "next/headers";
+
+export default async function DashboardRoute() {
+  const cookieStore = await cookies();
+  const loggedIn = cookieStore.has("token");
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
       <h1>Dashboard</h1>
+      <p>Logged in: {loggedIn ? "YES" : "NO"}</p>
     </div>
   );
 }
